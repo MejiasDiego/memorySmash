b36dd297e64fb0a433e132367b06b20090cf6890
ToniMGalera <toni.moreno.galera@gmail.com>
2024-05-21 13:49:56 +0200

    comida

---
+++ memorySmash/src/main/java/TrabajoGrupoMemory/Board.java
@@ -0,0 +1,31 @@
+package TrabajoGrupoMemory;
+
+package PruebaGrupal;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Board {
+    private ArrayList<Card> cards;
+
+    public Board() {
+        cards = new ArrayList<>();
+        ImageIcon defaultIcon = new ImageIcon(getClass().getResource("/resources/default.png"));
+        String[] imagePaths = {"/Banjo&KazooieHeadSSBUWebsite.png", "/CloudHeadSSBUWebsite.png", "/HeroHeadSSBUWebsite.png", "/JokerHeadSSBUWebsite.png", 
+                               "/Pac-ManHeadSSBUWebsite.png", "/SnakeHeadSSBUWebsite.png", "/SteveHeadSSBUWebsite.png", "/YoshiHeadSSBUWebsite.png"};
+
+        for (String path : imagePaths) {
+            ImageIcon image = new ImageIcon(getClass().getResource(path));
+            cards.add(new Card(image, defaultIcon));
+            cards.add(new Card(image, defaultIcon)); // Duplicar para tener parejas
+        }
+
+        Collections.shuffle(cards);
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+}
---
+++ memorySmash/src/main/java/TrabajoGrupoMemory/Card.java
@@ -0,0 +1,5 @@
+package TrabajoGrupoMemory;
+
+public class Card {
+
+}
